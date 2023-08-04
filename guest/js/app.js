58ed2b811d9e95dc9b30809ad32521b9d0202c2e
Tanya <117917951+tuna0007@users.noreply.github.com>
2023-08-04 15:25:30 +0300

    Delete guest directory

--- guest/js/app.js
+++
@@ -1,8 +0,0 @@
-// window.addEventListener("scroll", function() {
-//     const parallaxBlocks = document.querySelectorAll(".rooms-item");
-//     parallaxBlocks.forEach(function(block) {
-//     const speed = 0.15;
-//     const yPos = window.pageYOffset * speed;
-//     block.style.backgroundPosition = "center " + yPos + "px";
-//     });
-// });
