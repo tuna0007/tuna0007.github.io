776c0dda3b8404ee3cf8ccba224d7ad243b51121
T4HR <117917951+tuna0007@users.noreply.github.com>
2023-08-10 12:58:30 +0300

    Delete guestHouse directory

--- guestHouse/js/dropdown.js
+++
@@ -1,7 +0,0 @@
-function show(a){
-    document.querySelector('.drop-toggle').value = a;
-}
-let dropdown = document.querySelector('.drop');
-dropdown.onclick = function(){
-    dropdown.classList.toggle('active')
-}
