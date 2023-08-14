334885c8e90cc4ecead9712a544b292cab04bb95
T4HR <117917951+tuna0007@users.noreply.github.com>
2023-08-14 15:15:50 +0300

    sss

---
+++ guestHouse/src/js/app.js
@@ -0,0 +1,8 @@
+// window.addEventListener("scroll", function() {
+//     const parallaxBlocks = document.querySelectorAll(".rooms-item");
+//     parallaxBlocks.forEach(function(block) {
+//     const speed = 0.15;
+//     const yPos = window.pageYOffset * speed;
+//     block.style.backgroundPosition = "center " + yPos + "px";
+//     });
+// });
