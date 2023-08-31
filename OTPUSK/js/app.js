d13b2fa2ce833951da69e483794274c36f918c2b
T4HR <117917951+tuna0007@users.noreply.github.com>
2023-08-31 19:55:23 +0300

    Update app.js

--- OTPUSK/js/app.js
+++ OTPUSK/js/app.js
@@ -1,9 +1,9 @@
 const countdown = document.querySelector('.timer');
-const departure = new Date('2023-09-28:00:00');
+const departure = new Date('2023-09-07:00:00');
 
 function getSecondsToVacation() {
     let now = new Date();
-    let departure = new Date(2023, 7, 28);
+    let departure = new Date(2023, 9, 7);
     let res = departure - now;
     let seconds = Math.round(res / 1000);
     document.getElementById('seconds').innerText = seconds.toString().padStart(2, '0');
